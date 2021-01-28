71bb9a690f5bd2ebac3e34ff6c067489794c4ee4
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-01-28 22:02:00 +0530

    Update NUMPLIN.py

--- INPRG01/NUMPLIN.py
+++ INPRG01/NUMPLIN.py
@@ -1,3 +1,7 @@
+#You're given a number N, now check if the number N is palindrome or not.
+#Print "YES"(without quotes) if it's a palindrome, otherwise print "NO" (without quotes).
+
+
 N = int(input())
 original_N = N
 result = 0 
@@ -9,4 +13,10 @@ if result == original_N:
     print("YES")
 else:
     print("NO")
+    
+    
+#Sample Input 1:
+#8668
+#Sample Output 1:
+#YES
     
