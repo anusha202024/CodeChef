ee572843c7620e4509218a952c797e35e91e72a8
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-01-28 22:07:42 +0530

    Update CLODIV.py

--- INPRG01/CLODIV.py
+++ INPRG01/CLODIV.py
@@ -1,6 +1,16 @@
+#You are given two integers A and B. 
+#Print the number closest to A but less than or equal to A which is completely divisible by B.
+
 A = int(input())
 B = int(input())
 if A<B :
 	print("0")
 else :
 	print( (A//B)*B )
+	
+	
+#Sample Input 1:
+#23
+#7
+#Sample Output 1:
+#21
