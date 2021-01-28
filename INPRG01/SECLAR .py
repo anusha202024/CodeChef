fbd4f32cb1e10a3fa107568e1073e2e1aec2545f
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-01-28 22:04:15 +0530

    Update SECLAR .py

--- INPRG01/SECLAR .py
+++ INPRG01/SECLAR .py
@@ -1,3 +1,7 @@
+#You are given three distinct integers A, B, and C.
+#Find the 2′nd largest number among them and print it.
+
+
 A = int(input())
 B = int(input())
 C = int(input())
@@ -15,3 +19,12 @@ elif B>A:
 	print(B)
 else:
     print(A)
+
+
+#Sample Input 2:
+#14
+#28
+#16
+
+#Sample Output 2:
+#16
