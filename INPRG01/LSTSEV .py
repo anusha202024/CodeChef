63a443b8d534121232720d9a9f3fbfda215259cb
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-01-28 22:03:05 +0530

    Update LSTSEV .py

--- INPRG01/LSTSEV .py
+++ INPRG01/LSTSEV .py
@@ -1,5 +1,15 @@
+#Given a number N and check if its second last digit is 7 or not.
+#If it's 7 then print 1, otherwise print 0 in a single line
+
+
 N = int(input())
 if (N//10)%10 == 7:
 	print(1) 
 else:
 	print(0)
+	
+	
+#Sample Input 2:
+#4176
+#Sample Output 2:
+#1
