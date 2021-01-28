d6d40b98961abea1ac2246049f1bdb41f6c4ddde
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-01-28 21:43:46 +0530

    Update EOMUL.py

--- INPRG01/EOMUL.py
+++ INPRG01/EOMUL.py
@@ -1,3 +1,9 @@
+#Odd Even Multiple 
+
+#You are given an integer N. If N is not divisible by 3 then print −1.
+#If N is an odd multiple of 3 the print 1
+#and if N is an even multiple of 3 then print 0.
+
 N =int(input())
 if N%3 != 0:
     print("-1")
@@ -5,13 +11,8 @@ elif N%6 == 0:
     print("0")
 else:
     print("1")
-  
-
-
-
-
-  
-
     
-   
-  
+#Sample Input 1:
+#24
+#Sample Output 1:
+#0
