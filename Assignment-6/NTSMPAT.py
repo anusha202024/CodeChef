c2de9891977f49821383cb26e78ef5b967e87f54
Anusha <77925539+anusha202024@users.noreply.github.com>
2021-03-21 05:54:50 +0530

    Delete Assignment-6 directory

--- Assignment-6/NTSMPAT.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-for row in range(1 , N+1):
-    for i in range(row , row*2 ):
-        print(i , end = " ")
-    for i in reversed(range(row , row*2-1 )):
-        print(i , end = " ")
-    print(" ")
